155f942ec4c9d6d166fb53888cd1c9ffde31ad7e
cjeremey-rgb <cjeremey@hotmail.com>
2026-09-02 21:36:13 -0400

    Close gap in startup artwork composition

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='flip-rush-7-v2026-09-03-expanded-startup-art-v7';
+const CACHE_NAME='flip-rush-7-v2026-09-03-composed-startup-art-v7';
 const APP_SHELL = [
   './',
   './index.html',
