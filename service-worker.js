a64985af0dd934f20b05093d7ce230855b93bd5d
cjeremey-rgb <cjeremey@hotmail.com>
2026-09-02 21:29:44 -0400

    Expand startup artwork to fill viewport

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='flip-rush-7-v2026-09-03-full-screen-startups-v5';
+const CACHE_NAME='flip-rush-7-v2026-09-03-expanded-startup-art-v6';
 const APP_SHELL = [
   './',
   './index.html',
